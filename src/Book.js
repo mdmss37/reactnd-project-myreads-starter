1023a3927e1ce760e03147258babc7503869a069
Nakajima Kazuya <uniss010@gmail.com>
2017-08-01 14:46:03 +0900

    Update Book and BookShelf to be separate component to be rendered

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,38 @@
 import React, { Component } from "react"
 
-class Book extends from Component {
+class Book extends Component {
 
+  state = {
+    shelf: this.props.shelf,
+    book: this.props.book
+  }
+
+  updateShelf = (shelf) => {
+    this.setState({ shelf: shelf })
+  }
+
+  render() {
+    const { book, shelf } = this.state
+
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+          <div className="book-shelf-changer">
+            <select value={this.state.shelf} onClick={(e) => this.updateShelf(e.target.value)}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors}</div>
+      </div>
+    );
+  }
 }
+
+export default Book
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import Book from "./Book"
 
 class BookShelf extends Component {
 
@@ -10,23 +11,11 @@ class BookShelf extends Component {
         <div className="bookshelf-books">
           <ol className="books-grid">
             {this.props.books.map((book) => (
-              <li>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.authors}</div>
-                </div>
+              <li key={book.id}>
+                <Book
+                  book={book}
+                  shelf={book.shelf}
+                />
               </li>
               ))}
           </ol>
