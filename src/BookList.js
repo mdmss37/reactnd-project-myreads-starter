8f4df374f146f763ff7ec087f1209d076dc45ece
Nakajima Kazuya <uniss010@gmail.com>
2017-08-01 16:57:13 +0900

    Implement Link to transfer to Search page

--- src/BookList.js
+++ src/BookList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import { Link } from "react-router-dom"
 import BookShelf from "./BookShelf"
 
 class BookList extends Component {
@@ -30,7 +31,7 @@ class BookList extends Component {
           />
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to="/search">Add a book</Link>
         </div>
         {JSON.stringify(books.slice(0,10))}
       </div>
